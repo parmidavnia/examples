f1491ef6375ead0c928f18dc1f809eb7a40d2150
Parmida Vahdatnia <41003006+parmidavnia@users.noreply.github.com>
2020-08-15 17:50:23 -0700

    Update and rename hardware_script to hardware_script.cpp

--- hardware_script.cpp
+++ hardware_script.cpp
@@ -1,6 +1,6 @@
 
 input: debug_enable_list=List([node_id],[i_0,i_1,...]), expected_list = List([node_id],[i_0,i_1,...],[guard_value]) 
-// op_list= list of the nodes givne as input to the algorithm, for first tun op_list={store_ID[], br_ID[], ret_ID}. for other runs, it is the list of nodes
+// list of the nodes givne as input to the algorithm, for first tun op_list={store_ID[], br_ID[], ret_ID, carry_dep[]}
 output: memorybuffer= List(datapacket) //nodes that resulted in the wrong result
 datapacket = List([node_id],[i_0,i_1,...],[helper values of the node])
 //the first 8 left bits of the data packet are given to the sw algorithm as op_list input. the rest are for additional information for the user in saved faulty nodes  
